ff7c4c24b66559dbcfe695d323450f4245e6bb91
jason Duque <jasonD2212@outlook.com>
2021-07-25 17:51:36 -0600

    -Adding capablity to select another date to determine date time diffrence. -Including my initial thoughts of app

--- calcjs.js
+++ calcjs.js
@@ -1,38 +1,75 @@
-/*This function populates elements from calchtml.html with the current date YYYY-MM-DD 
+/*This function populates elements from calchtml.html with the current date YYYY-MM-DD HH:mm:ss 
 when the page first loads  */
 window.onload = function () {
-    // etriving current year, month, and day
+ 
+    // Retriving current Date and time in individual variables
     let currentYear = moment().year();
     // Need to add 1 becuase moment.month is zero based, returns 0-11
     var currentMonth = moment().month() + 1;
     let currentDate = moment().date();
+    let currentHour = moment().hours() ;
+    let currentMin = moment().minutes();
+    let currentSec = moment().format('ss');
+    
 
     // Populating HTML elements with values from above
     $('input[name="year"]').val(currentYear);
     $('input[name="month"]').val(currentMonth);
     $('input[name="date"]').val(currentDate);
+    $('input[name="hour"]').val(currentHour);
+    $('input[name="minute"]').val(currentMin);
+    $('input[name="second"]').val(currentSec);
+
+   
 }
 
-/*This function calculates the date diffrence between the current date and a selected date */
+/*This function calculates the datetime diffrence between the current datetime and a selected datetime */
 function calculate() {
-    // Revriving current date time object, time starts at beggining of current day
-    const today = moment().startOf('day');
-    // Retriving selected date from user input
-    let selectedYear = $('input[name="year"]').val();
-    let selectedMonth = $('input[name="month"]').val();
-    let selectedDate = $('input[name="date"]').val();
+    
+    // Retriving first selected date from user input
+    let firstSelectedYear = $('.firstDate input[name="year"]').val();
+    let firstSelectedMonth = $('.firstDate input[name="month"]').val();
+    let firstSelectedDate = $('.firstDate input[name="date"]').val();
+    let firstSelectedHour = $('.firstDate input[name="hour"]').val();
+    let firstSelectedMin = $('.firstDate input[name="minute"]').val();
+    let firstSelectedSec = $('.firstDate input[name="second"]').val();
+    
+    //// Retriving second selected date from user input
+    let secondSelectedYear = $('.secondDate input[name="year"]').val();
+    let secondSelectedMonth = $('.secondDate input[name="month"]').val();
+    let secondSelectedDate = $('.secondDate input[name="date"]').val();
+    let secondSelectedHour = $('.secondDate input[name="hour"]').val();
+    let secondSelectedMin = $('.secondDate input[name="minute"]').val();
+    let secondSelectedSec = $('.secondDate input[name="second"]').val();
     // Formatting selected date to YYYY-MM-DD
-    var selectedMoment = moment(selectedYear + '-' + selectedMonth + '-' + selectedDate, 'YYYY-MM-DD');
+    var firstSelectedMoment = moment(firstSelectedYear + '-' + firstSelectedMonth + '-' + firstSelectedDate + '' + firstSelectedHour + ':' + firstSelectedMin +':' + firstSelectedSec, 'YYYY-MM-DD, h:mm:ss');
+    var secondSelectedMoment = moment(secondSelectedYear + '-' + secondSelectedMonth + '-' + secondSelectedDate  + '' + secondSelectedHour + ':' + secondSelectedMin +':' + secondSelectedSec, 'YYYY-MM-DD, h:mm:ss');
+    // Holding un-modified second selected date for final output message
+    var secondMomentNEdit = moment(secondSelectedMoment, 'YYYY-MM-DD, h:mm:ss');
+   
+    
 
 /*Below: Determining diffrence for selected and current dates by individual date part . The ORDER in which we use .add() MATTERS! 
-today values change when calculating the diffrence for Month and Day depending on user input. We start with todays date
-and calculate what date values we need to get to our selected date */
-    var years = selectedMoment.diff(today, 'year');
-    today.add(years, 'years');
-    var months = selectedMoment.diff(today, 'months');
-    today.add(months, 'months');
-    var days = selectedMoment.diff(today, 'days');
+firstSelectedMoment values change when calculating the diffrence for Month and Day depending on user input. We start with todays date
+and calculate what datetime values we need to get to our second selected date */
+    var years = secondSelectedMoment.diff(firstSelectedMoment, 'year');
+    firstSelectedMoment.add(years, 'years');
+    var months = secondSelectedMoment.diff(firstSelectedMoment, 'months');
+    firstSelectedMoment.add(months, 'months');
+    var days = secondSelectedMoment.diff(firstSelectedMoment, 'days');
+    // Time calculations
+    var hours = secondSelectedMoment.diff(firstSelectedMoment, 'hours');
+    firstSelectedMoment.add(hours, 'hours');
+    var minutes = secondSelectedMoment.diff(firstSelectedMoment, 'minutes');
+    firstSelectedMoment.add(minutes, 'minutes');
+    var seconds = secondSelectedMoment.diff(firstSelectedMoment, 'seconds');
 
     //Output above values to HTML elements
-    $('#difference').html(years + 'Y ' + months + 'M ' + days + 'D');
+    $('#difference').html('Difference between ' + firstSelectedMoment.format('MMMM Do YYYY, h:mm:ss a') 
+    + ' and '+ secondMomentNEdit.format('MMMM Do YYYY, h:mm:ss a') + ' is: '+ years + 'Y ' + months + 'M ' + days + 'D ' + hours + 'H ' + minutes + 'm ' + seconds + 's');
+}
+
+/*Reloads the page*/
+function startover() {
+    location.reload();
 }
